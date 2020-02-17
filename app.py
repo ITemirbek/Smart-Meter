a2805160a1eaa74c4d81df0ba89afb65bbfe0290
Жаныбеков Нартай Ертаевич <zhanybekov_n@bi.group>
2020-02-17 16:29:37 +0600

    add navbar pages

--- app.py
+++ app.py
@@ -7,3 +7,22 @@ from flask import render_template
 def base():
     return render_template('main.html')
     # return render_template('hello.html')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/graph')
+def graph():
+    return render_template('graphs.html')
+    
+@app.route('/weather')
+def weather():
+    return render_template('weather.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
