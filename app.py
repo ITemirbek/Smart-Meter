2f1cc6f28453a95a2dcca45b8041102737271d72
Темірбек Исламбек <temirbek_is@bi.group>
2020-07-06 01:01:47 +0600

    deleted some musor:

--- app.py
+++ app.py
@@ -2,16 +2,9 @@ from flask import Flask
 from flask import render_template, request
 import requests
 import json
-from flask_socketio import SocketIO, send
-#Can be deleted: Only to check
 import time
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecret'
-socketio = SocketIO(app)
-# url should be localhost of the machine
-# url = "http://192.168.1.84:1080/api/all/all/now"
-
 
 
 @app.route('/')
@@ -66,41 +59,6 @@ def settings():
 	return render_template('settings.html')
 
 
-
-# @socketio.on('my event')
-# def handle_my_custom_event(msg):
-# 	# data = requests.get(url)
-# 	# data_json =  json.loads(data.text)
-# 	# phase_data = {}
-# 	# data_types = ['current', 'voltage', 'power', 'cosphi', 'frequency']
-# 	# for phase in range(3): 
-# 	#   phase_meter = data_json['datasets'][0]['phases'][phase]
-# 	#   phase_data['phase_{}'.format(phase)] = {}
-# 	#   print('phase',phase)
-# 	#   for meter in range(5):
-# 	#     phase_data['phase_{}'.format(phase)][data_types[meter]] = phase_meter['values'][meter]['data']
-# 	#     print(phase_meter['values'][meter]['data'])
-# 	print('Message: ' + msg)
-# 	phase_meter = [0.1, 5, 0.5, 0.86, 840] 
-
-# 	phase_data = {}
-# 	data_types = ['current', 'voltage', 'power', 'cosphi', 'frequency']
-
-# 	for phase in range(3): 
-# 	  phase_data['phase_{}'.format(phase)] = {}
-	  
-# 	  for meter in range(5):
-# 	    phase_data['phase_{}'.format(phase)][data_types[meter]] = phase_meter[meter]
-
-# 	emit('some event', phase_data, broadcast=True)
-
-@socketio.on('message')
-def handleMessage(msg):
-	print('Message: ' + msg)
-	send(msg, broadcast=True)
-
 	
 if __name__ == "__main__":
-	# app.run()
-	socketio.run(app)
-	# socketio.run(app, host='0.0.0.0')
+	app.run()
